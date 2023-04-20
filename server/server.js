a7e8b3e281e91e65a47e94751d66bc2f7ee984ae
Devesh Sharma <devesh.s785dds@gmail.com>
2023-04-20 18:13:59 +0530

    Your commit message here

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import { app } from "./app.js";
 import { config } from "dotenv";
 import mongoose from "mongoose";
-
 import cloudinary from "cloudinary"
 
 config({
@@ -14,14 +13,17 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // we should always connect database after connecting config
-
-
-
 mongoose.connect(process.env.MONGO_URI).then(()=>{console.log(`MongoDb connected`)})
 console.log(process.env.MONGO_URI)
 
-  
 const port = process.env.PORT || 4000;
 
 app.set('port', port);
@@ -30,8 +32,6 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-
 const server = app.listen(port, () => {
   console.log(`Server is running on PORT ${server.address().port}`);
 });
-
