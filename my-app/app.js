731406bc22975fbddab4f2790c803e97486bae39
Devesh Sharma <devesh.s785dds@gmail.com>
2023-04-20 18:43:26 +0530

    Your commit message here

--- my-app/app.js
+++ my-app/app.js
@@ -1,8 +1,17 @@
-import Main from "./main";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Main from "./Main";
+import axios from "axios";
 
 export default function App() {
+  useEffect(() => {
+    axios
+      .get("https://todo007.onrender.com")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Provider store={store}>
       <Main />
