ceda54cfa9388a094bc9a2d4f02ee12d30e075b3
Devesh Sharma <devesh.s785dds@gmail.com>
2023-04-20 20:02:12 +0530

    Your commit message here

--- server/app.js
+++ server/app.js
@@ -1,35 +1,22 @@
 import express from "express";
-import User from "./routers/User.js";
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
-import cors from "cors";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import Main from "./main";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-// create express app
 const app = express();
 
-// set up middleware
-app.use(express.json()); // will convert string into obj
-app.use(express.urlencoded({ extended: false })); // its a middleware function
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
-app.use(
-  fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-    useTempFiles: true,
-  })
-);
 app.use(cors());
 
-// get directory name of the current module file
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// serve static files from frontend
-app.use(express.static(path.join(__dirname, "../my-app/build")));
-
-// serve frontend app on root route
+// serve your React Native app as the root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../my-app/build", "index.html"));
+  res.send(
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 });
 
 // set up API routes
