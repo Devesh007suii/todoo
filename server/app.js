9f5597fde733d4f46e0cd526113d02c69b2a6368
Devesh Sharma <devesh.s785dds@gmail.com>
2023-04-20 21:17:29 +0530

    Your commit message here

--- server/app.js
+++ server/app.js
@@ -1,44 +1,26 @@
-require('babel-register')({
-  presets: ['@babel/preset-react']
-});
-
 import express from "express";
-import React from "react";
-import { renderToString } from "react-dom/server";
-import Main from "../my-app/main";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import User from "./routers/User.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    useTempFiles: true,
+  })
+);
 app.use(cors());
 
-// serve your React Native app as the root route
-app.get("/", (req, res) => {
-  const appHtml = renderToString(
-    <Provider store={store}>
-      <Main />
-    </Provider>
-  );
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>React Native App</title>
-      </head>
-      <body>
-        <div id="app">${appHtml}</div>
-      </body>
-    </html>
-  `);
-});
-
-// set up API routes
 app.use("/api/v1", User);
+
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
 
 export default app;
