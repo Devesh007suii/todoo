c7882c9ffc3f71e0d1ef7d55a94f41d836355b7a
Devesh Sharma <devesh.s785dds@gmail.com>
2023-04-20 19:34:12 +0530

    Your commit message here

--- server/server.js
+++ server/server.js
@@ -1,37 +1,44 @@
-import { app } from "./app.js";
 import { config } from "dotenv";
 import mongoose from "mongoose";
 import cloudinary from "cloudinary"
 import app from "./app.js";
 
-
 config({
-    path:"./config/config.env",
-})
+  path: "./config/config.env",
+});
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 // Enable CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // we should always connect database after connecting config
-mongoose.connect(process.env.MONGO_URI).then(()=>{console.log(`MongoDb connected`)})
-console.log(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`MongoDb connected`);
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
 
 const port = process.env.PORT || 4000;
 
-app.set('port', port);
+app.set("port", port);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 const server = app.listen(port, () => {
